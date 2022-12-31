49f47b48b22fd8901b4a95f8e34d4bee9f5f01b8
mehmetsirin <mhmtsirinsk@gmail.com>
2022-12-31 17:26:49 +0300

    test

--- OctopusTest/OctopusTest/Controllers/WeatherForecastController.cs
+++ OctopusTest/OctopusTest/Controllers/WeatherForecastController.cs
@@ -14,7 +14,7 @@ namespace OctopusTest.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
