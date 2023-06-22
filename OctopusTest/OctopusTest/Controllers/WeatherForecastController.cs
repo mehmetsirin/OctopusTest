e4501d20d92934e389b99cb7a3123f5443f1c0aa
mehmetsirin <mhmtsirinsk@gmail.com>
2023-06-22 16:18:36 +0300

    dsd

--- OctopusTest/OctopusTest/Controllers/WeatherForecastController.cs
+++ OctopusTest/OctopusTest/Controllers/WeatherForecastController.cs
@@ -23,7 +23,7 @@ namespace OctopusTest.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         { 
-            AppDomain  
+            AppDomain    main
                   mehmet  
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
