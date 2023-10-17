8befd58a74536b3e038d80f0eeb1b0a513cfbae6
mehmetsirin <mhmtsirinsk@gmail.com>
2023-10-17 17:42:05 +0300

    ewe

--- OctopusTest/OctopusTest/WeatherForecast.cs
+++ OctopusTest/OctopusTest/WeatherForecast.cs
@@ -4,12 +4,4 @@ namespace OctopusTest
 {
     public class WeatherForecast
     {
-        int main = 0;
-        int main1 =1 ;
-        int  ccc
-
-            int sdsd
-
-efrerer     
-            ererere
 }
